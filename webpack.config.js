dc6581eff1d0f3703ff211c935d25f53d3f16f8a
Leonid Trukhin <truhin.leo@yandex.ru>
2025-03-17 18:38:16 +1000

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+HtmlWebpackPlugin = require('HtmlWebpackPlugin')
+path = require('path')
+
+module.exports = {
+  entry: './app.js', // Точка входа для сборки проекта
+
+  output: {
+    filename: 'bundle.js', // Имя выходного файла сборки
+    path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+      },
+    ],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './popup.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    open: true, // Автоматически открывать браузер
+  },
+
+  mode: 'development', // Режим сборки
+};
